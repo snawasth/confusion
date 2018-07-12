289d14f6112ec1f363a9ddf9cdccd9c3d8d302e7
snawasth <sneha.awasthi@capgemini.com>
2018-07-12 11:53:33 +0530

    Components Part 2 Added

--- src/App.js
+++ src/App.js
@@ -3,21 +3,28 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap'; 
 import Menu from './components/MenuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
   render() {
     return (
-      <div className="App">
-        
-       <Navbar dark color="primary">
+      <div>
+      <Navbar dark color="secondary">
        <div className="container">
        <NavbarBrand href="/">WebDev</NavbarBrand>
        </div>
        </Navbar>
-
-       <Menu />
-      </div>
-    );
+     <Menu dishes={this.state.dishes} />
+     </div>
+      );
   }
 }
 
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Media } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class Menu extends Component {
 
@@ -7,58 +7,50 @@ class Menu extends Component {
         super(props);
 
         this.state = {
-            dishes: [
-                {
-                    id: 0,
-                    name:'NodeJs',
-                    image: 'assets/images/nodejs.png',
-                    category: 'mains',
-                    label:'Hot',
-                    price:'4.99',
-                    description:'Lorem ipsum dolor sit amet, eu autem alterum platonem vel, liber utamur malorum sea cu.'                        },
-                 {
-                    id: 1,
-                    name:'Jquery',
-                    image: 'assets/images/jquery.png',
-                    category: 'appetizer',
-                    label:'',
-                    price:'1.99',
-                    description:'Lorem ipsum dolor sit amet, eu autem alterum platonem vel, liber utamur malorum sea cu.'                        },
-                 {
-                    id: 2,
-                    name:'ReactJs',
-                    image: 'assets/images/reactjs.png',
-                    category: 'appetizer',
-                    label:'New',
-                    price:'1.99',
-                    description:'Lorem ipsum dolor sit amet, eu autem alterum platonem vel, liber utamur malorum sea cu.'                        },
-                 {
-                    id: 3,
-                    name:'MongoDb',
-                    image: 'assets/images/mongodb.png',
-                    category: 'dessert',
-                    label:'',
-                    price:'2.99',
-                    description:'Lorem ipsum dolor sit amet, eu autem alterum platonem vel, liber utamur malorum sea cu.'                        
-                }
-            ]
+            selectedDish: null
+            
         }
     }
     
+    onDishSelect(dish){
+        this.setState({ selectedDish: dish});
+    }
+
+    renderDish(dish){
+        if (dish != null){
+            return(
+                <Card>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardBody>
+                <CardTitle>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+                    </CardBody>
+                    </Card>
+            );
+        }
+        else{
+            return(
+                <div></div>
+            );
+        }
+    }
+
     render() {
 
-        const menu = this.state.dishes.map((dish) => {
+        const menu = this.props.dishes.map((dish) => {
             return(
-                <div key={dish.id} className="col-12 mt-5">
-                    <Media tag="li">
-                        <Media left middle>
-                            <Media object src={dish.image} alt={dish.name} />
-                            </Media>
-                            <Media body className="ml-5">
-                                <Media heading>{dish.name}</Media>
-                                <p>{dish.description}</p>
-                                </Media>
-                        </Media>
+                <div key={dish.id} className="col-12 col-md-5 m-1">
+                    <Card onClick={() => this.onDishSelect(dish)}>
+                        
+                            <CardImg width="100%" src={dish.image} alt={dish.name} />
+                          
+                            <CardImgOverlay>
+                                <CardTitle>{dish.name}</CardTitle>
+                              
+                                
+
+                                </CardImgOverlay>
+                        </Card>
                     </div>
             );
 
@@ -67,8 +59,11 @@ class Menu extends Component {
         return ( 
                 <div className="container">
                     <div className="row">
-                        <Media list>{menu}</Media>
+                        {menu}
                         </div>
+                        <div className="row">
+                            {this.renderDish(this.state.selectedDish)}
+                            </div>
                         </div>
 
         );
