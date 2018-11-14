ad2d570429e9ac2a452ad0adb37a436a6d7175c1
snawasth <sneha.awasthi@capgemini.com>
2018-11-14 14:06:34 +0530

    new push

---
+++ GitPullBatch.bat
@@ -0,0 +1,8 @@
+@echo off
+REM call git add .
+REM set /p id="Enter Commit Description: "\r\n
+REM call git commit -m "%id%"
+REM echo Wait for Process to complete.....
+REM call git push https://github.com/grimmhuntress/Test1.git
+call git pull https://github.com/snawasth/confusion.git master
+pause
